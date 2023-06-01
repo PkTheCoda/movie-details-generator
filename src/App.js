32630af9a2994f284a54439a27b2ddbac0bc9a76
PkTheCoda <107774675+PkTheCoda@users.noreply.github.com>
2023-06-01 12:10:48 -0400

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
-import "./style.css";
+import "./styles.css";
+import { useState } from 'react'
 
 export default function App() {
+
+  const [input, setInput] = useState("")
+
+  function handleInputVal(event) {
+    const typedVal = event.target.value
+    setInput(typedVal)
+    console.log('Typed value:', typedVal);
+
+  }
+
+  async function getMovieDetails(input) {
+    let data = await fetch(`https://www.omdbapi.com/?t=${input}&apikey=43282252`, {
+      mode: 'cors',
+    })
+  
+    let parsedData = await data.json()
+    console.log(parsedData)
+  }
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <div className="header--container">
+
+        <div className="input-modal-container">
+          <h1 className="main--header">Movie Details Generator</h1>
+
+          <div className="input--container">
+            <input className="api--input" type="text" onChange={handleInputVal}/>
+            <button className="submit--button" onClick={() => getMovieDetails(input)}>Click me</button>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className="body--container">
+        <h1>Test</h1>
+      </div>
+    </>
   );
 }
